1da6fe2006623bc34493e826fcf8447c645a7286
Puneetprakashsharma <sharmapuneetprakash07@gmail.com>
2023-04-21 19:39:44 +0530

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # js_ecommerce
 Ecommerce by using Javascript, html &amp; css
+
+Added files
---
+++ js/ecommerce.js
@@ -0,0 +1,5 @@
+function myimg(filename){
+    let img = document.getElementById("img_change");
+    img.setAttribute("src",filename);
+}
+
