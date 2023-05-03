712b3a5a9e77e0088e20555591c483a4597a0853
Puneetprakashsharma <sharmapuneetprakash07@gmail.com>
2023-05-03 22:27:37 +0530

    Updated project

--- README.md
+++
@@ -1,4 +0,0 @@
-# js_ecommerce
-Ecommerce by using Javascript, html &amp; css
-
-Added files
--- js_signup/signup.js
+++
@@ -1,7 +0,0 @@
-function myfunc(){
-    alert("THANK YOU FOR VISITING")
-}
-function myvideo(){
-    prompt("If you are happy with the service type YES")
-    alert("THANK YOU FOR VISITING")
-}
